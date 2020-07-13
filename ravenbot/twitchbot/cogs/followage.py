d87ed833e71b632905f5c785d1c8482b7338493c
dreaded_ <tim.squadrito@gmail.com>
2020-07-13 18:29:34 -0400

    Update followage.py

--- ravenbot/twitchbot/cogs/followage.py
+++ ravenbot/twitchbot/cogs/followage.py
@@ -1,6 +1,15 @@
 from twitchio.ext import commands
 
 
+def format_date(self, s):
+    date = '-'.split(s)
+    year = date[0]
+    month = date[1]
+    day = date[2]
+    formatted = f"{month}/{day}/{year}"
+    return formatted
+
+
 @commands.cog()
 class Followage:
 
@@ -12,7 +21,7 @@ class Followage:
             tosend = f"@{ctx.author.name} you cannot follow yourself"
         elif len(user_data) == 2:
             follow = await self.bot.get_follow(user_data[0].id, user_data[1].id)
-            date = self.format_date(follow["followed_at"].split("T")[0])
+            date = format_date(follow["followed_at"].split("T")[0])
             tosend = f"@{ctx.author.name} has been following {ctx.channel.name} since {date}"
         else:
             tosend = 'error'
@@ -22,12 +31,3 @@ class Followage:
     async def followage(self, ctx):
         user_data = await self.bot.get_users(ctx.author.name, ctx.channel.name)
         await self.get_follow(ctx, user_data)
-
-    def format_date(self, s):
-        date = '-'.split(s)
-        year = date[0]
-        month = date[1]
-        day = date[2]
-        formatted = f"{month}/{day}/{year}"
-        return formatted
-
